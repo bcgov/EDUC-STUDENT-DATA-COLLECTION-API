6b16095a9ec86ebdbf32aabe3d21192bcd8d7535
SoLetsDev <74216496+SoLetsDev@users.noreply.github.com>
2023-02-15 07:46:28 -0800

    added in delete collection test and changed application.properties in test. Should help CI/CD run

--- api/src/test/java/ca/bc/gov/educ/studentdatacollection/api/collection/v1/CollectionControllerTest.java
+++ api/src/test/java/ca/bc/gov/educ/studentdatacollection/api/collection/v1/CollectionControllerTest.java
@@ -2,6 +2,7 @@ package ca.bc.gov.educ.studentdatacollection.api.collection.v1;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -49,26 +50,24 @@ public class CollectionControllerTest {
 
   @BeforeEach
   public void setUp() {
-    System.out.println("RUNNING SETUP");
     MockitoAnnotations.openMocks(this);
   }
 
   @BeforeEach
   public void before() {
-    System.out.println("RUNNING BEFORE");
     this.collectionCodeRepository.save(this.createCollectionCodeData());
     this.collectionRepository.save(this.createCollectionData());
   }
 
   @AfterEach
   public void after() {
-    System.out.println("RUNNING AFTER");
     this.collectionRepository.deleteAll();
     this.collectionCodeRepository.deleteAll();
+
   }
 
   @Test
-  public void testGetAllCollections_WithWrongScope_ShouldReturnForbidden() throws Exception {
+  public void testGetAllCollections_WithWrongScope_ShouldReturnStatusForbidden() throws Exception {
     final GrantedAuthority grantedAuthority = () -> "WRONG_SCOPE";
     final OidcLoginRequestPostProcessor mockAuthority = oidcLogin().authorities(grantedAuthority);
     this.mockMvc.perform(get(URL.BASE_URL_COLLECTION).with(mockAuthority)).andDo(print())
@@ -119,6 +118,68 @@ public class CollectionControllerTest {
     this.mockMvc.perform(post(URL.BASE_URL_COLLECTION).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON).content(asJsonString(collection)).with(mockAuthority))
         .andDo(print()).andExpect(status().isCreated());
+  }
+
+  @Test
+  public void testCreateCollection_WithWrongScope_ShouldReturnStatusForbidden() throws Exception {
+    final GrantedAuthority grantedAuthority = () -> "WRONG_SCOPE";
+    final OidcLoginRequestPostProcessor mockAuthority = oidcLogin().authorities(grantedAuthority);
+
+    final CollectionEntity collection = this.createCollectionData();
+    collection.setCreateDate(null);
+    collection.setUpdateDate(null);
+
+    this.mockMvc.perform(post(URL.BASE_URL_COLLECTION).contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON).content(asJsonString(collection)).with(mockAuthority))
+        .andDo(print()).andExpect(status().isForbidden());
+  }
+
+  @Test
+  public void testCreateCollection_WithInvalidData_ShouldReturnStatusBadRequest() throws Exception {
+    final GrantedAuthority grantedAuthority = () -> "SCOPE_WRITE_SDC_COLLECTION";
+    final OidcLoginRequestPostProcessor mockAuthority = oidcLogin().authorities(grantedAuthority);
+
+    final CollectionEntity collection = this.createCollectionData();
+
+    this.mockMvc.perform(post(URL.BASE_URL_COLLECTION).contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON).content(asJsonString(collection)).with(mockAuthority))
+        .andDo(print()).andExpect(status().isBadRequest());
+  }
+
+  @Test
+  public void testDeleteCollection_ShouldReturnStatusNoContent() throws Exception {
+    final GrantedAuthority grantedAuthority = () -> "SCOPE_DELETE_SDC_COLLECTION";
+    final OidcLoginRequestPostProcessor mockAuthority = oidcLogin().authorities(grantedAuthority);
+
+    final CollectionEntity newCollection = this.collectionRepository.save(
+        this.createCollectionData());
+
+    this.mockMvc.perform(
+            delete(URL.BASE_URL_COLLECTION + "/" + newCollection.getCollectionID()).with(mockAuthority))
+        .andDo(print()).andExpect(status().isNoContent());
+  }
+
+  @Test
+  public void testDeleteCollection_WithWrongID_ShouldReturnStatusNotFound() throws Exception {
+    final GrantedAuthority grantedAuthority = () -> "SCOPE_DELETE_SDC_COLLECTION";
+    final OidcLoginRequestPostProcessor mockAuthority = oidcLogin().authorities(grantedAuthority);
+
+    this.mockMvc.perform(
+            delete(URL.BASE_URL_COLLECTION + "/" + UUID.randomUUID()).with(mockAuthority))
+        .andDo(print()).andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void testDeleteCollection_WithWrongScope_ShouldReturnStatusForbidden() throws Exception {
+    final GrantedAuthority grantedAuthority = () -> "WRONG_SCOPE";
+    final OidcLoginRequestPostProcessor mockAuthority = oidcLogin().authorities(grantedAuthority);
+
+    final CollectionEntity collection = this.createCollectionData();
+    collection.setCreateDate(null);
+    collection.setUpdateDate(null);
+
+    this.mockMvc.perform(delete(URL.BASE_URL_COLLECTION + "/" + UUID.randomUUID()).with(mockAuthority))
+        .andDo(print()).andExpect(status().isForbidden());
   }
 
   private CollectionCodeEntity createCollectionCodeData() {
