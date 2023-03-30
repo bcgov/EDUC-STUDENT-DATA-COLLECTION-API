b81a676fe8400dd956a21490a0f84e40cfb9a9ce
arcshiftsolutions <arcshiftsolutions@gmail.com>
2023-03-30 12:46:19 -0700

    Change for performance.

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/controller/v1/SdcFileController.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/controller/v1/SdcFileController.java
@@ -3,6 +3,8 @@ package ca.bc.gov.educ.studentdatacollection.api.controller.v1;
 import ca.bc.gov.educ.studentdatacollection.api.batch.service.SdcFileService;
 import ca.bc.gov.educ.studentdatacollection.api.endpoint.v1.SdcFileEndpoint;
 import ca.bc.gov.educ.studentdatacollection.api.mappers.v1.SdcSchoolCollectionMapper;
+import ca.bc.gov.educ.studentdatacollection.api.model.v1.SdcSchoolCollectionStudentEntity;
+import ca.bc.gov.educ.studentdatacollection.api.service.v1.SdcService;
 import ca.bc.gov.educ.studentdatacollection.api.struct.v1.SdcFileUpload;
 import ca.bc.gov.educ.studentdatacollection.api.struct.v1.SdcSchoolCollection;
 import ca.bc.gov.educ.studentdatacollection.api.struct.v1.SdcStudentCount;
@@ -12,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -22,15 +25,21 @@ public class SdcFileController implements SdcFileEndpoint {
 
   private final SdcSchoolCollectionFileValidator sdcSchoolCollectionFileValidator;
 
-  public SdcFileController(SdcFileService sdcFileService, SdcSchoolCollectionFileValidator sdcSchoolCollectionFileValidator) {
+  private final SdcService sdcService;
+
+  public SdcFileController(SdcFileService sdcFileService, SdcSchoolCollectionFileValidator sdcSchoolCollectionFileValidator, SdcService sdcService) {
     this.sdcFileService = sdcFileService;
     this.sdcSchoolCollectionFileValidator = sdcSchoolCollectionFileValidator;
+    this.sdcService = sdcService;
   }
 
   @Override
   public ResponseEntity<SdcSchoolCollection> processSdcBatchFile(SdcFileUpload fileUpload, String correlationID) {
     ValidationUtil.validatePayload(() -> this.sdcSchoolCollectionFileValidator.validatePayload(fileUpload));
     var sdcSchoolCollectionEntity = sdcFileService.runFileLoad(fileUpload);
+    List<SdcSchoolCollectionStudentEntity> mainStudentList = new ArrayList<>();
+    mainStudentList.addAll(sdcSchoolCollectionEntity.getSDCSchoolStudentEntities());
+    this.sdcService.prepareAndSendSdcStudentsForFurtherProcessing(mainStudentList);
     return ResponseEntity.ok(SdcSchoolCollectionMapper.mapper.toSdcSchoolBatch(sdcSchoolCollectionEntity));
   }
 
