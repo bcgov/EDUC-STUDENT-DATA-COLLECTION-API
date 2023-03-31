1250c6bf7e9ccf6b06202bed5f62d475a23106f9
arcshiftsolutions <arcshiftsolutions@gmail.com>
2023-03-31 08:39:38 -0700

    Performance.

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/config/AsyncConfiguration.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/config/AsyncConfiguration.java
@@ -38,7 +38,7 @@ public class AsyncConfiguration {
   public Executor controllerTaskExecutor() {
     return new EnhancedQueueExecutor.Builder()
       .setThreadFactory(new ThreadFactoryBuilder().withNameFormat("async-executor-%d").get())
-      .setCorePoolSize(16).setMaximumPoolSize(16).setKeepAliveTime(Duration.ofSeconds(60)).build();
+      .setCorePoolSize(4).setMaximumPoolSize(8).setKeepAliveTime(Duration.ofSeconds(60)).build();
   }
 
   @Bean(name = "publisherExecutor")
