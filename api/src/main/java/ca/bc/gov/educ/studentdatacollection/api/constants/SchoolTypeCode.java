af12bf0ef2f2e0926feabb7632666f8f8ef4ad1c
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2023-04-18 13:54:58 -0700

    revert

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/constants/SchoolTypeCode.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/constants/SchoolTypeCode.java
@@ -1,22 +1,12 @@
 package ca.bc.gov.educ.studentdatacollection.api.constants;
 
-import java.util.Arrays;
-import java.util.Optional;
-
 /**
  * this contains different codes to handle the flow for particular school types.
  */
 public enum SchoolTypeCode {
-  PUBLIC("PUBLIC"),
-  INDEPEND("INDEPEND"),
-  OFFSHORE("OFFSHORE");
-
-  private final String code;
-  SchoolTypeCode(String code) {
-    this.code = code;
-  }
-
-  public static Optional<SchoolTypeCode> findByValue(String schoolCategoryCode) {
-    return Arrays.stream(values()).filter(typeCode -> typeCode.code.equals(schoolCategoryCode)).findFirst();
-  }
+  DEFAULT,
+  SUMMER_SCHOOL,
+  SFAS,
+  PSI,
+  NONE // this is for myed-api flow.
 }
