282393c548b8bc7f4f46a1f581b5fd2da04b79ab
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2023-04-26 12:25:34 -0700

    code table endpoints + ut

---
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/controller/v1/CodeTableAPIController.java
@@ -0,0 +1,50 @@
+package ca.bc.gov.educ.studentdatacollection.api.controller.v1;
+
+import ca.bc.gov.educ.studentdatacollection.api.endpoint.v1.CodeTableAPIEndpoint;
+import ca.bc.gov.educ.studentdatacollection.api.mappers.v1.CodeTableMapper;
+import ca.bc.gov.educ.studentdatacollection.api.service.v1.CodeTableService;
+import ca.bc.gov.educ.studentdatacollection.api.struct.v1.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+public class CodeTableAPIController implements CodeTableAPIEndpoint {
+    private final CodeTableService codeTableService;
+    private static final CodeTableMapper mapper = CodeTableMapper.mapper;
+
+    public CodeTableAPIController(CodeTableService codeTableService) {
+        this.codeTableService = codeTableService;
+    }
+
+    @Override
+    public List<EnrolledProgramCode> getEnrolledProgramCodes() {
+        return codeTableService.getAllEnrolledProgramCodes().stream().map(mapper::toStructure).toList();
+    }
+
+    @Override
+    public List<CareerProgramCode> getCareerProgramCodes() {
+        return codeTableService.getAllCareerProgramCodes().stream().map(mapper::toStructure).toList();
+    }
+    @Override
+    public List<HomeLanguageSpokenCode> getHomeLanguageSpokenCodes() {
+        return codeTableService.getAllHomeLanguageSpokenCodes().stream().map(mapper::toStructure).toList();
+    }
+
+    @Override
+    public List<BandCode> getBandCodes() {
+        return codeTableService.getAllBandCodes().stream().map(mapper::toStructure).toList();
+    }
+
+    @Override
+    public List<SchoolFundingCode> getFundingCodes() {
+        return codeTableService.getAllFundingCodes().stream().map(mapper::toStructure).toList();
+    }
+
+    @Override
+    public List<EnrolledGradeCode> getEnrolledGradeCodes() {
+        return codeTableService.getAllGradeCodes().stream().map(mapper::toStructure).toList();
+    }
+}
---
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/endpoint/v1/CodeTableAPIEndpoint.java
@@ -0,0 +1,67 @@
+package ca.bc.gov.educ.studentdatacollection.api.endpoint.v1;
+
+import ca.bc.gov.educ.studentdatacollection.api.constants.v1.URL;
+import ca.bc.gov.educ.studentdatacollection.api.model.v1.HomeLanguageSpokenCodeEntity;
+import ca.bc.gov.educ.studentdatacollection.api.struct.v1.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping(URL.BASE_URL)
+public interface CodeTableAPIEndpoint {
+
+    @PreAuthorize("hasAuthority('SCOPE_READ_COLLECTION_CODES')")
+    @GetMapping(URL.ENROLLED_PROGRAM_CODES)
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
+    @Transactional(readOnly = true)
+    @Tag(name = "Collection Codes", description = "Endpoints to get collection codes.")
+    @Schema(name = "EnrolledProgramCode", implementation = EnrolledProgramCode.class)
+    List<EnrolledProgramCode> getEnrolledProgramCodes();
+
+    @PreAuthorize("hasAuthority('SCOPE_READ_COLLECTION_CODES')")
+    @GetMapping(URL.CAREER_PROGRAM_CODES)
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
+    @Transactional(readOnly = true)
+    @Tag(name = "Collection Codes", description = "Endpoints to get collection codes.")
+    @Schema(name = "CareerProgramCode", implementation = CareerProgramCode.class)
+    List<CareerProgramCode> getCareerProgramCodes();
+
+    @PreAuthorize("hasAuthority('SCOPE_READ_COLLECTION_CODES')")
+    @GetMapping(URL.HOME_LANGUAGE_SPOKEN_CODES)
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
+    @Transactional(readOnly = true)
+    @Tag(name = "Collection Codes", description = "Endpoints to get collection codes.")
+    @Schema(name = "HomeLanguageSpokenCode", implementation = HomeLanguageSpokenCode.class)
+    List<HomeLanguageSpokenCode> getHomeLanguageSpokenCodes();
+
+    @PreAuthorize("hasAuthority('SCOPE_READ_COLLECTION_CODES')")
+    @GetMapping(URL.BAND_CODES)
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
+    @Transactional(readOnly = true)
+    @Tag(name = "Collection Codes", description = "Endpoints to get collection codes.")
+    @Schema(name = "BandCode", implementation = BandCode.class)
+    List<BandCode> getBandCodes();
+
+    @PreAuthorize("hasAuthority('SCOPE_READ_COLLECTION_CODES')")
+    @GetMapping(URL.FUNDING_CODES)
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
+    @Transactional(readOnly = true)
+    @Tag(name = "Collection Codes", description = "Endpoints to get collection codes.")
+    @Schema(name = "SchoolFundingCode", implementation = SchoolFundingCode.class)
+    List<SchoolFundingCode> getFundingCodes();
+
+    @PreAuthorize("hasAuthority('SCOPE_READ_COLLECTION_CODES')")
+    @GetMapping(URL.GRADE_CODES)
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
+    @Transactional(readOnly = true)
+    @Tag(name = "Collection Codes", description = "Endpoints to get collection codes.")
+    @Schema(name = "EnrolledGradeCode", implementation = EnrolledGradeCode.class)
+    List<EnrolledGradeCode> getEnrolledGradeCodes();
+}
