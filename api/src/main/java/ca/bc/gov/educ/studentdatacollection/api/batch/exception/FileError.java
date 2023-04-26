1f0e45820203a17df03086a9f962e1fa3bcb19fe
Trevor Richards <trev@trevdev.ca>
2023-04-26 12:43:26 -0700

    chore: add a little whitespace

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/batch/exception/FileError.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/batch/exception/FileError.java
@@ -12,26 +12,32 @@ public enum FileError {
    * The Invalid transaction code header.
    */
   INVALID_TRANSACTION_CODE_HEADER("Invalid transaction code on Header record. It must be FFI"),
+
   /**
    * The Invalid transaction code trailer.
    */
   INVALID_TRANSACTION_CODE_TRAILER("Invalid transaction code on Trailer record. It must be BTR"),
+
   /**
    * The Invalid mincode header.
    */
   INVALID_MINCODE_HEADER("Invalid Mincode in Header record."),
+
   /**
    * The Student count mismatch.
    */
   STUDENT_COUNT_MISMATCH("Invalid count in trailer record. Stated was $?, Actual was $?"),
+
   /**
    * Invalid trailer
    */
   INVALID_TRAILER("Invalid trailer record. Student count could not be retrieved"),
+
   /**
    * Invalid trailer for student count
    */
   INVALID_TRAILER_STUDENT_COUNT("Invalid trailer record. Student count was not a numeric value"),
+
   /**
    * The Invalid transaction code student details.
    */
@@ -42,6 +48,7 @@ public enum FileError {
    * This will be thrown when any row in the given file is longer or shorter than expected.
    */
   INVALID_ROW_LENGTH("$?"),
+
   /**
    * The mincode school is currently closed
    */
