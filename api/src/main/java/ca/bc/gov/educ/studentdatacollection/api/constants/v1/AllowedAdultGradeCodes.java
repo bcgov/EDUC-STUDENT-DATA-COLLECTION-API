d3b12d06515a1b64f991f4b097184accb738e674
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2023-04-28 12:12:13 -0700

    Update AllowedAdultGradeCodes.java

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/constants/v1/AllowedAdultGradeCodes.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/constants/v1/AllowedAdultGradeCodes.java
@@ -3,9 +3,7 @@ package ca.bc.gov.educ.studentdatacollection.api.constants.v1;
 import lombok.Getter;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public enum AllowedAdultGradeCodes {
     GRADE10("10"),
