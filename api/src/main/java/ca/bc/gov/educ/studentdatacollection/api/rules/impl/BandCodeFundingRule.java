d5bb8fd347bdcf008f67b690b24a51cf553b4e0c
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2023-07-21 13:11:15 -0700

    Update BandCodeFundingRule.java

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/rules/impl/BandCodeFundingRule.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/rules/impl/BandCodeFundingRule.java
@@ -6,7 +6,6 @@ import ca.bc.gov.educ.studentdatacollection.api.constants.SdcSchoolCollectionStu
 import ca.bc.gov.educ.studentdatacollection.api.constants.v1.CollectionTypeCodes;
 import ca.bc.gov.educ.studentdatacollection.api.constants.v1.Constants;
 import ca.bc.gov.educ.studentdatacollection.api.rules.BaseRule;
-import ca.bc.gov.educ.studentdatacollection.api.service.v1.ValidationRulesService;
 import ca.bc.gov.educ.studentdatacollection.api.struct.SdcStudentSagaData;
 import ca.bc.gov.educ.studentdatacollection.api.struct.v1.SdcSchoolCollectionStudentValidationIssue;
 import org.apache.commons.lang3.StringUtils;
