006b7c9a1e1f4df34afb7a10544dd8602d24774f
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2023-08-15 11:15:55 -0700

    code smell fix

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/controller/v1/SdcSchoolCollectionStudentController.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/controller/v1/SdcSchoolCollectionStudentController.java
@@ -23,8 +23,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
