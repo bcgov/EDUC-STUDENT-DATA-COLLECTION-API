f38f00793d291a369155e333d4e451b1e28fbfd7
Trevor Richards <trev@trevdev.ca>
2023-08-18 14:49:21 -0700

    fix: unfix fixed code smell

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/constants/SdcSchoolCollectionStudentProgramEligibilityIssueCode.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/constants/SdcSchoolCollectionStudentProgramEligibilityIssueCode.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 
 abstract class Constants {
   private Constants() {}
-  public static final String notEnrolled = "NOENROLLED";
+  public static final String NOT_ENROLLED = "NOENROLLED";
 }
 
 public enum SdcSchoolCollectionStudentProgramEligibilityIssueCode {
@@ -22,9 +22,9 @@ public enum SdcSchoolCollectionStudentProgramEligibilityIssueCode {
     + " years to be eligible for this program."),
   INACTIVE_SCHOOL_AGE("INACTMINOR", "School aged students must have been reported to have 1 or more courses in the"
     + " last two years to be eligible for this program."),
-  NOT_ENROLLED_FRENCH(Constants.notEnrolled, "The student is not enrolled with French programming."),
-  NOT_ENROLLED_CAREER(Constants.notEnrolled, "The student is not enrolled with career programming."),
-  NOT_ENROLLED_INDIGENOUS(Constants.notEnrolled, "The student is not enrolled with indigeneous programming."),
+  NOT_ENROLLED_FRENCH(Constants.NOT_ENROLLED, "The student is not enrolled with French programming."),
+  NOT_ENROLLED_CAREER(Constants.NOT_ENROLLED, "The student is not enrolled with career programming."),
+  NOT_ENROLLED_INDIGENOUS(Constants.NOT_ENROLLED, "The student is not enrolled with indigeneous programming."),
   DOES_NOT_NEED_SPECIAL_ED("NOSPECIAL", "The student does not require special education assistance."),
   IS_GRADUATED("GRADUATED", "Student must be school-aged or a non-graduated adult to be eligible for "
     + "Special Education funding."),
