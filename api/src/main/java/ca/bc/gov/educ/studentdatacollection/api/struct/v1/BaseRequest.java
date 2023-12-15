18f3bedf2965e65fcb2c64af470f951b4808b8c2
Marco Villeneuve <arcshiftsolutions@gmail.com>
2023-12-15 10:49:53 -0800

    Fixes for audit column length.

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/struct/v1/BaseRequest.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/struct/v1/BaseRequest.java
@@ -12,9 +12,9 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 public abstract class BaseRequest {
-    @Size(max = 32)
+    @Size(max = 100)
     protected String createUser;
-    @Size(max = 32)
+    @Size(max = 100)
     protected String updateUser;
     @Null(message = "createDate should be null.")
     protected String createDate;
