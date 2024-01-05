05c19c082f72750f18115749b4a79eb68c916a0b
Marco Villeneuve <arcshiftsolutions@gmail.com>
2024-01-04 16:41:29 -0800

    Coverage.

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/service/v1/ValidationRulesService.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/service/v1/ValidationRulesService.java
@@ -32,15 +32,18 @@ public class ValidationRulesService {
     private final SdcSchoolCollectionStudentRepository sdcSchoolStudentRepository;
     private final RestUtils restUtils;
     private static final CodeTableMapper mapper = CodeTableMapper.mapper;
+
     public ValidationRulesService(CodeTableService codeTableService, SdcStudentEllRepository sdcStudentEllRepository, SdcSchoolCollectionStudentRepository sdcSchoolStudentRepository, RestUtils restUtils) {
         this.codeTableService = codeTableService;
         this.sdcStudentEllRepository = sdcStudentEllRepository;
         this.sdcSchoolStudentRepository = sdcSchoolStudentRepository;
         this.restUtils = restUtils;
     }
+
     public Long getDuplicatePenCount(UUID sdcSchoolID, String studentPen) {
         return sdcSchoolStudentRepository.countForDuplicateStudentPENs(sdcSchoolID, studentPen);
     }
+
     public List<EnrolledProgramCode> getActiveEnrolledProgramCodes() {
         return codeTableService.getAllEnrolledProgramCodes().stream().filter(code -> code.getExpiryDate().isAfter(LocalDateTime.now())).map(mapper::toStructure).toList();
     }
@@ -48,14 +51,16 @@ public class ValidationRulesService {
     public List<CareerProgramCode> getActiveCareerProgramCodes() {
         return codeTableService.getAllCareerProgramCodes().stream().filter(code -> code.getExpiryDate().isAfter(LocalDateTime.now())).map(mapper::toStructure).toList();
     }
+
     public List<HomeLanguageSpokenCode> getActiveHomeLanguageSpokenCodes() {
         return codeTableService.getAllHomeLanguageSpokenCodes().stream().filter(code -> code.getExpiryDate().isAfter(LocalDateTime.now())).map(mapper::toStructure).toList();
     }
-    public  List<BandCode> getActiveBandCodes() {
+
+    public List<BandCode> getActiveBandCodes() {
         return codeTableService.getAllBandCodes().stream().filter(code -> code.getExpiryDate().isAfter(LocalDateTime.now())).map(mapper::toStructure).toList();
     }
 
-    public  List<SchoolFundingCode> getActiveFundingCodes() {
+    public List<SchoolFundingCode> getActiveFundingCodes() {
         return codeTableService.getAllFundingCodes().stream().filter(code -> code.getExpiryDate().isAfter(LocalDateTime.now())).map(mapper::toStructure).toList();
     }
 
@@ -74,67 +79,71 @@ public class ValidationRulesService {
     }
 
     public List<String> splitString(String enrolledProgramCode) {
-        if(StringUtils.isEmpty(enrolledProgramCode)) {
+        if (StringUtils.isEmpty(enrolledProgramCode)) {
             return Collections.<String>emptyList();
         }
         return Pattern.compile(".{1,2}").matcher(enrolledProgramCode).results().map(MatchResult::group).toList();
     }
 
-    public Optional<SdcStudentEllEntity> getStudentYearsInEll(String studentID){
+    public Optional<SdcStudentEllEntity> getStudentYearsInEll(String studentID) {
         return sdcStudentEllRepository.findByStudentID(UUID.fromString(studentID));
     }
 
-  public void updatePenMatchAndGradStatusColumns(SdcSchoolCollectionStudentEntity student, String mincode) throws EntityNotFoundException {
-    var penMatchResult = this.restUtils.getPenMatchResult(UUID.randomUUID(), student, mincode);
-    val penMatchResultCode = penMatchResult.getPenStatus();
-    student.setPenMatchResult(penMatchResultCode);
-    var validPenMatchResults = Arrays.asList("AA", "B1", "C1", "D1");
-    var multiPenMatchResults = Arrays.asList("BM", "CM", "DM");
+    public void updatePenMatchAndGradStatusColumns(SdcSchoolCollectionStudentEntity student, String mincode) throws EntityNotFoundException {
+        var penMatchResult = this.restUtils.getPenMatchResult(UUID.randomUUID(), student, mincode);
+        val penMatchResultCode = penMatchResult.getPenStatus();
+        student.setPenMatchResult(penMatchResultCode);
+        var validPenMatchResults = Arrays.asList("AA", "B1", "C1", "D1");
+        var multiPenMatchResults = Arrays.asList("BM", "CM", "DM");
 
-    if (StringUtils.isNotEmpty(penMatchResultCode) && validPenMatchResults.contains(penMatchResultCode)) {
-      final var penMatchRecordOptional = penMatchResult.getMatchingRecords().stream().findFirst();
-      if (penMatchRecordOptional.isPresent()) {
-        var assignedPEN = penMatchRecordOptional.get().getMatchingPEN();
-        var assignedStudentID = penMatchRecordOptional.get().getStudentID();
+        if (StringUtils.isNotEmpty(penMatchResultCode) && validPenMatchResults.contains(penMatchResultCode)) {
+            if (penMatchResult.getMatchingRecords() == null) {
+                log.error("PEN Match records list is null - this should not have happened :: ", penMatchResult);
+                throw new StudentDataCollectionAPIRuntimeException("PEN Match records list is null - this should not have happened :: ");
+            }
+            final var penMatchRecordOptional = penMatchResult.getMatchingRecords().stream().findFirst();
+            if (penMatchRecordOptional.isPresent()) {
+                var assignedPEN = penMatchRecordOptional.get().getMatchingPEN();
+                var assignedStudentID = penMatchRecordOptional.get().getStudentID();
 
-        student.setAssignedStudentId(UUID.fromString(assignedStudentID));
-        student.setAssignedPen(assignedPEN);
-      } else {
-        log.error("PenMatchRecord in priority queue is empty for matched status, this should not have happened.");
-        throw new StudentDataCollectionAPIRuntimeException("PenMatchRecord in priority queue is empty for matched status, this should not have happened.");
-      }
-    }else if (StringUtils.isNotEmpty(penMatchResultCode) && multiPenMatchResults.contains(penMatchResultCode)) {
-      student.setPenMatchResult("MULTI");
-    }else{
-      student.setPenMatchResult("NEW");
-    }
+                student.setAssignedStudentId(UUID.fromString(assignedStudentID));
+                student.setAssignedPen(assignedPEN);
+            } else {
+                log.error("PenMatchRecord in priority queue is empty for matched status, this should not have happened.");
+                throw new StudentDataCollectionAPIRuntimeException("PenMatchRecord in priority queue is empty for matched status, this should not have happened.");
+            }
+        } else if (StringUtils.isNotEmpty(penMatchResultCode) && multiPenMatchResults.contains(penMatchResultCode)) {
+            student.setPenMatchResult("MULTI");
+        } else {
+            student.setPenMatchResult("NEW");
+        }
 
 //    if(student.getAssignedStudentId() != null) {
 //        final var gradResult = this.restUtils.getGradStatusResult(UUID.randomUUID(), SdcSchoolCollectionStudentMapper.mapper.toSdcSchoolStudent(student));
 //        log.info("Grad status for SDC student {} :: is {}", student.getSdcSchoolCollectionStudentID(), gradResult);
 //    }
-    student.setIsGraduated(false);
-  }
+        student.setIsGraduated(false);
+    }
 
-  public boolean hasEnrollmentHistory(StudentRuleData studentRuleData){
-      var student = studentRuleData.getSdcSchoolCollectionStudentEntity();
-      var school = studentRuleData.getSchool();
-      var twoYearsAgo = student.getSdcSchoolCollection().getCollectionEntity().getOpenDate().getYear() - 2;
+    public boolean hasEnrollmentHistory(StudentRuleData studentRuleData) {
+        var student = studentRuleData.getSdcSchoolCollectionStudentEntity();
+        var school = studentRuleData.getSchool();
+        var twoYearsAgo = student.getSdcSchoolCollection().getCollectionEntity().getOpenDate().getYear() - 2;
 
 
-      var listOfNumCoursesLastTwoYears = getSdcSchoolStudentRepository().getCollectionHistory(UUID.fromString(school.getSchoolId()),
-              student.getStudentPen(), student.getSdcSchoolCollection().getCollectionEntity().getOpenDate(), studentRuleData.getCollectionTypeCode(), twoYearsAgo);
+        var listOfNumCoursesLastTwoYears = getSdcSchoolStudentRepository().getCollectionHistory(UUID.fromString(school.getSchoolId()),
+                student.getStudentPen(), student.getSdcSchoolCollection().getCollectionEntity().getOpenDate(), studentRuleData.getCollectionTypeCode(), twoYearsAgo);
 
-      for (String numString : listOfNumCoursesLastTwoYears){
-          try{
-              if (Integer.parseInt(numString) > 0) {
-                  return true;
-              }
-          } catch (Exception e) {
-              //Do nothing
-          }
-      }
+        for (String numString : listOfNumCoursesLastTwoYears) {
+            try {
+                if (Integer.parseInt(numString) > 0) {
+                    return true;
+                }
+            } catch (Exception e) {
+                //Do nothing
+            }
+        }
 
-      return false;
+        return false;
     }
 }
--- api/src/test/java/ca/bc/gov/educ/studentdatacollection/api/service/v1/ValidationRulesServiceTest.java
+++ api/src/test/java/ca/bc/gov/educ/studentdatacollection/api/service/v1/ValidationRulesServiceTest.java
@@ -1,6 +1,7 @@
 package ca.bc.gov.educ.studentdatacollection.api.service.v1;
 
 import ca.bc.gov.educ.studentdatacollection.api.BaseStudentDataCollectionAPITest;
+import ca.bc.gov.educ.studentdatacollection.api.exception.StudentDataCollectionAPIRuntimeException;
 import ca.bc.gov.educ.studentdatacollection.api.model.v1.SdcSchoolCollectionStudentEntity;
 import ca.bc.gov.educ.studentdatacollection.api.rest.RestUtils;
 import ca.bc.gov.educ.studentdatacollection.api.struct.external.penmatch.v1.PenMatchResult;
@@ -8,6 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -38,6 +41,28 @@ class ValidationRulesServiceTest extends BaseStudentDataCollectionAPITest {
 
         assertNull(mockStudentEntity.getAssignedStudentId());
         assertSame("MULTI", mockStudentEntity.getPenMatchResult());
+    }
+
+    @Test
+    void testGetPenMatchResultErrorThrown() {
+        SdcSchoolCollectionStudentEntity mockStudentEntity = new SdcSchoolCollectionStudentEntity();
+        PenMatchResult penMatchResult = getPenMatchResult();
+        penMatchResult.setPenStatus("D1");
+        penMatchResult.setMatchingRecords(new ArrayList<>());
+        when(this.restUtils.getPenMatchResult(any(),any(), anyString())).thenReturn(penMatchResult);
+
+        assertThrows(StudentDataCollectionAPIRuntimeException.class, () -> validationRulesService.updatePenMatchAndGradStatusColumns(mockStudentEntity, "123456789"));
+    }
+
+    @Test
+    void testGetPenMatchResultErrorThrownNull() {
+        SdcSchoolCollectionStudentEntity mockStudentEntity = new SdcSchoolCollectionStudentEntity();
+        PenMatchResult penMatchResult = getPenMatchResult();
+        penMatchResult.setPenStatus("D1");
+        penMatchResult.setMatchingRecords(null);
+        when(this.restUtils.getPenMatchResult(any(),any(), anyString())).thenReturn(penMatchResult);
+
+        assertThrows(StudentDataCollectionAPIRuntimeException.class, () -> validationRulesService.updatePenMatchAndGradStatusColumns(mockStudentEntity, "123456789"));
     }
 
     @Test
