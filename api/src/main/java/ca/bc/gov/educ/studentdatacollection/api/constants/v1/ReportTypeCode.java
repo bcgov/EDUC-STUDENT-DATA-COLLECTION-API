f2387a0bd657821a31fd473c01ca2fd1466c32ca
Marco Villeneuve <arcshiftsolutions@gmail.com>
2024-02-16 12:06:03 -0800

    Coverage

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/constants/v1/ReportTypeCode.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/constants/v1/ReportTypeCode.java
@@ -3,9 +3,7 @@ package ca.bc.gov.educ.studentdatacollection.api.constants.v1;
 import lombok.Getter;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * The enum Event outcome.
@@ -24,7 +22,4 @@ public enum ReportTypeCode {
     return Arrays.stream(values()).filter(e -> Arrays.asList(e.code).contains(value)).findFirst();
   }
 
-  public static List<String> getCodes() {
-    return Arrays.stream(ReportTypeCode.values()).map(ReportTypeCode::getCode).collect(Collectors.toList());
-  }
 }
