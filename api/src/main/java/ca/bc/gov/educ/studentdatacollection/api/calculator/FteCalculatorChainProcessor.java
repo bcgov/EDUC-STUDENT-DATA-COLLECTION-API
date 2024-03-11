92d96db43d7c0cf1574a2938c78cd7c8126a9a22
mightycox <j.acox@hotmail.com>
2024-03-11 10:33:02 -0700

    Fixes bug in fte calculation when isGraduate was not set during validation checks

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/calculator/FteCalculatorChainProcessor.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/calculator/FteCalculatorChainProcessor.java
@@ -1,5 +1,6 @@
 package ca.bc.gov.educ.studentdatacollection.api.calculator;
 
+import ca.bc.gov.educ.studentdatacollection.api.service.v1.ValidationRulesService;
 import ca.bc.gov.educ.studentdatacollection.api.struct.StudentRuleData;
 import ca.bc.gov.educ.studentdatacollection.api.struct.v1.FteCalculationResult;
 import lombok.extern.slf4j.Slf4j;
@@ -13,14 +14,22 @@ import java.util.List;
 public class FteCalculatorChainProcessor {
 
     private final List<FteCalculator> fteCalculators;
+    private final ValidationRulesService validationRulesService;
 
     @Autowired
-    public FteCalculatorChainProcessor(final List<FteCalculator> fteCalculators) {
+    public FteCalculatorChainProcessor(final List<FteCalculator> fteCalculators, ValidationRulesService validationRulesService) {
         this.fteCalculators = fteCalculators;
+        this.validationRulesService = validationRulesService;
     }
 
     public FteCalculationResult processFteCalculator(StudentRuleData studentRuleData) {
         log.debug("Starting FTE calculation for student :: {} with data :: {}", studentRuleData.getSdcSchoolCollectionStudentEntity().getSdcSchoolCollectionStudentID(), studentRuleData);
+        var student = studentRuleData.getSdcSchoolCollectionStudentEntity();
+        if(student.getIsGraduated() == null) {
+            log.debug("Fetching isGraduated for student :: {}", student.getSdcSchoolCollectionStudentID());
+            validationRulesService.updatePenMatchAndGradStatusColumns(student, studentRuleData.getSchool().getMincode());
+        }
+
         for (int i = 0; i < fteCalculators.size() - 1; i++) {
             FteCalculator currentCalculator = fteCalculators.get(i);
             FteCalculator nextCalculator = fteCalculators.get(i + 1);
