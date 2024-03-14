eb32242b59cf952b56dbfcb44f90a7374d10cfe1
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2024-03-14 15:01:37 -0700

    remove debug

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/rules/programelegibility/impl/IndigenousSupportProgramsRule.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/rules/programelegibility/impl/IndigenousSupportProgramsRule.java
@@ -47,8 +47,6 @@ public class IndigenousSupportProgramsRule implements ProgramEligibilityBaseRule
     String ancestryData = student.getNativeAncestryInd();
 
     log.debug("ProgramEligibilityBaseRule - IndigenousSupportProgramsRule: Enrolled Programs - {} - school -{} for sdcSchoolCollectionStudentID :: {}", studentPrograms, studentRuleData.getSchool(), studentRuleData.getSdcSchoolCollectionStudentEntity().getSdcSchoolCollectionStudentID());
-    log.debug("ProgramEligibilityBaseRule - IndigenousSupportProgramsRule: INDEPEND - {} ", studentRuleData.getSchool().getSchoolCategoryCode().equalsIgnoreCase(SchoolCategoryCodes.INDEPEND.getCode()));
-    log.debug("ProgramEligibilityBaseRule - IndigenousSupportProgramsRule: else if condition - {} ", studentRuleData.getSchool().getSchoolCategoryCode().equalsIgnoreCase(SchoolCategoryCodes.INDEPEND.getCode()) || studentRuleData.getSchool().getSchoolCategoryCode().equalsIgnoreCase(SchoolCategoryCodes.INDP_FNS.getCode()));
     if (EnrolledProgramCodes.getIndigenousProgramCodes().stream().noneMatch(studentPrograms::contains)) {
       errors.add(ProgramEligibilityIssueCode.NOT_ENROLLED_INDIGENOUS);
     } else if (Boolean.FALSE.equals(student.getIsSchoolAged())) {
@@ -58,7 +56,6 @@ public class IndigenousSupportProgramsRule implements ProgramEligibilityBaseRule
     } else if (studentRuleData.getSchool().getSchoolCategoryCode().equalsIgnoreCase(SchoolCategoryCodes.INDEPEND.getCode()) || studentRuleData.getSchool().getSchoolCategoryCode().equalsIgnoreCase(SchoolCategoryCodes.INDP_FNS.getCode())) {
       errors.add(ProgramEligibilityIssueCode.INDIGENOUS_INDY_SCHOOL);
     }
-    log.debug("ProgramEligibilityBaseRule - IndigenousSupportProgramsRule: Errors - {} ", errors);
     return errors;
   }
 
