f2bc06dd43b1a0f826d8e14be9a6e539a03a9df6
mightycox <j.acox@hotmail.com>
2024-04-04 12:24:38 -0700

    EDX-1750, EDX-1752, EDX-1766 - Adds endpoints for seeding AT dat EDX-1751 - Adds submissionDate to sdcDistrict struct to fix bug of missing submission date on district collection view

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/service/v1/SdcDistrictCollectionService.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/service/v1/SdcDistrictCollectionService.java
@@ -4,9 +4,12 @@ import ca.bc.gov.educ.studentdatacollection.api.constants.v1.SdcDistrictCollecti
 import ca.bc.gov.educ.studentdatacollection.api.exception.EntityNotFoundException;
 import ca.bc.gov.educ.studentdatacollection.api.model.v1.*;
 import ca.bc.gov.educ.studentdatacollection.api.repository.v1.*;
+import ca.bc.gov.educ.studentdatacollection.api.util.TransformUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -16,10 +19,12 @@ import java.util.UUID;
 public class SdcDistrictCollectionService {
 
   private final SdcDistrictCollectionRepository sdcDistrictCollectionRepository;
+  private final CollectionRepository collectionRepository;
 
   @Autowired
-  public SdcDistrictCollectionService(SdcDistrictCollectionRepository sdcDistrictCollectionRepository) {
+  public SdcDistrictCollectionService(SdcDistrictCollectionRepository sdcDistrictCollectionRepository, CollectionRepository collectionRepository) {
     this.sdcDistrictCollectionRepository = sdcDistrictCollectionRepository;
+    this.collectionRepository = collectionRepository;
   }
 
   public SdcDistrictCollectionEntity getSdcDistrictCollection(UUID sdcDistrictCollectionID) {
@@ -33,5 +38,22 @@ public class SdcDistrictCollectionService {
 
   public SdcDistrictCollectionEntity getActiveSdcDistrictCollectionByDistrictID(UUID districtID) {
     return sdcDistrictCollectionRepository.findByDistrictIDAndSdcDistrictCollectionStatusCodeNotIgnoreCase(districtID, SdcDistrictCollectionStatus.COMPLETED.getCode()).orElseThrow(() -> new EntityNotFoundException(SdcDistrictCollectionEntity.class, "Collection for district Id", districtID.toString()));
+  }
+
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
+  public SdcDistrictCollectionEntity createSdcDistrictCollectionByCollectionID(SdcDistrictCollectionEntity sdcDistrictCollectionEntity ,UUID collectionID) {
+    Optional<CollectionEntity> collectionEntityOptional = collectionRepository.findById(collectionID);
+    CollectionEntity collectionEntity = collectionEntityOptional.orElseThrow(() -> new EntityNotFoundException(CollectionEntity.class, "collectionID", collectionID.toString()));
+
+    TransformUtil.uppercaseFields(sdcDistrictCollectionEntity);
+    sdcDistrictCollectionEntity.setCollectionEntity(collectionEntity);
+    return sdcDistrictCollectionRepository.save(sdcDistrictCollectionEntity);
+  }
+
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
+  public void deleteSdcDistrictCollection(UUID sdcDistrictCollectionID) {
+    Optional<SdcDistrictCollectionEntity> entityOptional = sdcDistrictCollectionRepository.findById(sdcDistrictCollectionID);
+    SdcDistrictCollectionEntity entity = entityOptional.orElseThrow(() -> new EntityNotFoundException(SdcDistrictCollectionEntity.class, "sdcDistrictCollectionID", sdcDistrictCollectionID.toString()));
+    sdcDistrictCollectionRepository.delete(entity);
   }
 }
---
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/validator/SdcDistrictCollectionValidator.java
@@ -0,0 +1,54 @@
+package ca.bc.gov.educ.studentdatacollection.api.validator;
+
+import ca.bc.gov.educ.studentdatacollection.api.constants.v1.SdcDistrictCollectionStatus;
+import ca.bc.gov.educ.studentdatacollection.api.model.v1.SdcDistrictCollectionEntity;
+import ca.bc.gov.educ.studentdatacollection.api.repository.v1.SdcDistrictCollectionRepository;
+import ca.bc.gov.educ.studentdatacollection.api.struct.v1.SdcDistrictCollection;
+import ca.bc.gov.educ.studentdatacollection.api.util.ValidationUtil;
+import lombok.AccessLevel;
+import lombok.Getter;
+import org.apache.commons.lang3.EnumUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.FieldError;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class SdcDistrictCollectionValidator {
+
+  public static final String DISTRICT_COLLECTION_ID = "sdcDistrictCollectionID";
+  public static final String DISTRICT_COLLECTION_STATUS_CODE = "sdcDistrictCollectionStatusCode";
+
+  @Getter(AccessLevel.PRIVATE)
+  private final SdcDistrictCollectionRepository sdcDistrictCollectionRepository;
+
+  @Autowired
+  public SdcDistrictCollectionValidator(SdcDistrictCollectionRepository sdcDistrictCollectionRepository) {
+    this.sdcDistrictCollectionRepository = sdcDistrictCollectionRepository;
+  }
+
+  public List<FieldError> validatePayload(SdcDistrictCollection sdcDistrictCollection, boolean isCreateOperation) {
+    final List<FieldError> apiValidationErrors = new ArrayList<>();
+    try {
+      if(isCreateOperation && sdcDistrictCollection.getSdcDistrictCollectionID() != null) {
+        apiValidationErrors.add(ValidationUtil.createFieldError(DISTRICT_COLLECTION_ID, sdcDistrictCollection.getSdcDistrictCollectionID(), "sdcDistrictCollectionID should be null for post operation."));
+      }
+      if(!isCreateOperation) {
+        Optional<SdcDistrictCollectionEntity> districtCollectionEntity = this.sdcDistrictCollectionRepository.findById(UUID.fromString(sdcDistrictCollection.getSdcDistrictCollectionID()));
+        if (districtCollectionEntity.isEmpty()) {
+          apiValidationErrors.add(ValidationUtil.createFieldError(DISTRICT_COLLECTION_ID, sdcDistrictCollection.getSdcDistrictCollectionID(), "Invalid SDC district collection ID."));
+        }
+      }
+      if (!EnumUtils.isValidEnum(SdcDistrictCollectionStatus.class, sdcDistrictCollection.getSdcDistrictCollectionStatusCode())) {
+        apiValidationErrors.add(ValidationUtil.createFieldError(DISTRICT_COLLECTION_STATUS_CODE, sdcDistrictCollection.getSdcDistrictCollectionStatusCode(), "Invalid SDC district collection status code."));
+      }
+    }catch(Exception e){
+      apiValidationErrors.add(ValidationUtil.createFieldError(DISTRICT_COLLECTION_STATUS_CODE, sdcDistrictCollection.getSdcDistrictCollectionStatusCode(), "Invalid SDC district collection status code."));
+    }
+    return apiValidationErrors;
+  }
+}
