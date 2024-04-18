8bbbf78b1d735703fcd764e092f2436f94c3bd35
Eckerman <erineckerman@gmail.com>
2024-04-18 15:42:11 -0700

    remove unused imports

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/repository/v1/SdcDistrictCollectionRepository.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/repository/v1/SdcDistrictCollectionRepository.java
@@ -2,10 +2,8 @@ package ca.bc.gov.educ.studentdatacollection.api.repository.v1;
 
 import ca.bc.gov.educ.studentdatacollection.api.model.v1.SdcDistrictCollectionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
