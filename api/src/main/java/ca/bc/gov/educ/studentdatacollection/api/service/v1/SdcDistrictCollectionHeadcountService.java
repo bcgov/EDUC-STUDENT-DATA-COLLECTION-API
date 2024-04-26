7e7db1a0f6f8a617c3d398500fd6a93c607f9f9f
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2024-04-26 12:31:14 -0700

    remove dup code

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/service/v1/SdcDistrictCollectionHeadcountService.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/service/v1/SdcDistrictCollectionHeadcountService.java
@@ -71,7 +71,7 @@ public class SdcDistrictCollectionHeadcountService {
     List<SpecialEdHeadcountResult> collectionRawDataForHeadcount = sdcSchoolCollectionStudentRepository.getSpecialEdHeadcountsBySchoolIdAndBySdcDistrictCollectionId(sdcDistrictCollectionID);
 
     HeadcountResultsTable collectionDataForHeadcounts = specialEdHeadcountHelper.convertHeadcountResultsToSchoolGradeTable(collectionRawDataForHeadcount);
-    List<HeadcountHeader> headcountHeaderList = specialEdHeadcountHelper.getHeadersPerGrade(sdcDistrictCollectionID);
+    List<HeadcountHeader> headcountHeaderList = specialEdHeadcountHelper.getHeaders(sdcDistrictCollectionID, true);
 
     return SdcSchoolCollectionStudentHeadcounts.builder().headcountHeaders(headcountHeaderList).headcountResultsTable(collectionDataForHeadcounts).build();
   }
