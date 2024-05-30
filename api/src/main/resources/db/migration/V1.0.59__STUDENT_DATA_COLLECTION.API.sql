91bdfe552eb9b948def17c0abb8e27915aac3197
Marco Villeneuve <arcshiftsolutions@gmail.com>
2024-05-30 14:20:58 -0700

    Updates for enrolled program descriptions

---
+++ api/src/main/resources/db/migration/V1.0.59__STUDENT_DATA_COLLECTION.API.sql
@@ -0,0 +1,9 @@
+UPDATE ENROLLED_PROGRAM_CODE
+SET LABEL = 'Youth Work in Trades Program',
+    DESCRIPTION = 'Youth Work in Trades Program'
+WHERE ENROLLED_PROGRAM_CODE = '42';
+
+UPDATE ENROLLED_PROGRAM_CODE
+SET LABEL = 'Career Tech Youth Train Trades',
+    DESCRIPTION = 'Career Technical or Youth Train in Trades'
+WHERE ENROLLED_PROGRAM_CODE = '43';
