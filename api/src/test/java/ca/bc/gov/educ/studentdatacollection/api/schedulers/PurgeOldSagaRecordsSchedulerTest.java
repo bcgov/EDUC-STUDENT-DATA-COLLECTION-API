7d696a7880f94dafd9bcbb1c6119b15a92984f33
Marco Villeneuve <arcshiftsolutions@gmail.com>
2024-06-06 12:56:03 -0700

    Cleanup.

--- api/src/test/java/ca/bc/gov/educ/studentdatacollection/api/schedulers/PurgeOldSagaRecordsSchedulerTest.java
+++ api/src/test/java/ca/bc/gov/educ/studentdatacollection/api/schedulers/PurgeOldSagaRecordsSchedulerTest.java
@@ -10,9 +10,10 @@ import ca.bc.gov.educ.studentdatacollection.api.repository.v1.SagaEventRepositor
 import ca.bc.gov.educ.studentdatacollection.api.repository.v1.SagaRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import static org.assertj.core.api.Assertions.assertThat;
+
 import java.time.LocalDateTime;
-import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PurgeOldSagaRecordsSchedulerTest extends BaseStudentDataCollectionAPITest {
 
