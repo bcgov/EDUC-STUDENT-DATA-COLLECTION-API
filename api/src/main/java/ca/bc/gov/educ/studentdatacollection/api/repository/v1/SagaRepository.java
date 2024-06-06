9db25bc53f7f2ad45acaa76bed788ce92db99a43
Marco Villeneuve <arcshiftsolutions@gmail.com>
2024-06-05 17:33:09 -0700

    Added scheduler to purge records

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/repository/v1/SagaRepository.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/repository/v1/SagaRepository.java
@@ -4,8 +4,12 @@ package ca.bc.gov.educ.studentdatacollection.api.repository.v1;
 import ca.bc.gov.educ.studentdatacollection.api.model.v1.SdcSagaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -22,4 +26,8 @@ public interface SagaRepository extends JpaRepository<SdcSagaEntity, UUID>, JpaS
 
   long countAllByStatusIn(List<String> statuses);
 
+  @Transactional
+  @Modifying
+  @Query("delete from SdcSagaEntity where createDate <= :createDate")
+  void deleteByCreateDateBefore(LocalDateTime createDate);
 }
