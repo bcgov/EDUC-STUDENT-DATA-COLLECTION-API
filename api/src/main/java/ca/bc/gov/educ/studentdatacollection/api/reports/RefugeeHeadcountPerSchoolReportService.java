d34174bb632141f17d8fdc15fbd03d0311b35091
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2024-06-26 10:33:21 -0700

    refugee per school report service

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/reports/RefugeeHeadcountPerSchoolReportService.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/reports/RefugeeHeadcountPerSchoolReportService.java
@@ -1,10 +1,22 @@
 package ca.bc.gov.educ.studentdatacollection.api.reports;
 
+import ca.bc.gov.educ.studentdatacollection.api.constants.v1.ReportTypeCode;
+import ca.bc.gov.educ.studentdatacollection.api.exception.EntityNotFoundException;
 import ca.bc.gov.educ.studentdatacollection.api.exception.StudentDataCollectionAPIRuntimeException;
+import ca.bc.gov.educ.studentdatacollection.api.model.v1.SdcDistrictCollectionEntity;
 import ca.bc.gov.educ.studentdatacollection.api.properties.ApplicationProperties;
+import ca.bc.gov.educ.studentdatacollection.api.repository.v1.SdcDistrictCollectionRepository;
+import ca.bc.gov.educ.studentdatacollection.api.repository.v1.SdcSchoolCollectionRepository;
+import ca.bc.gov.educ.studentdatacollection.api.repository.v1.SdcSchoolCollectionStudentRepository;
 import ca.bc.gov.educ.studentdatacollection.api.rest.RestUtils;
 import ca.bc.gov.educ.studentdatacollection.api.struct.v1.headcounts.RefugeeHeadcountResult;
+import ca.bc.gov.educ.studentdatacollection.api.struct.v1.reports.DownloadableReportResponse;
 import ca.bc.gov.educ.studentdatacollection.api.struct.v1.reports.HeadcountChildNode;
+import ca.bc.gov.educ.studentdatacollection.api.struct.v1.reports.HeadcountNode;
+import ca.bc.gov.educ.studentdatacollection.api.struct.v1.reports.HeadcountReportNode;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.jasperreports.engine.JRException;
@@ -13,16 +25,24 @@ import net.sf.jasperreports.engine.JasperReport;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
-import java.util.HashMap;
+import java.util.*;
 
 @Service
 @Slf4j
 public class RefugeeHeadcountPerSchoolReportService extends BaseReportGenerationService<RefugeeHeadcountResult> {
 
+    private final SdcSchoolCollectionRepository sdcSchoolCollectionRepository;
+    private final SdcDistrictCollectionRepository sdcDistrictCollectionRepository;
+    private final SdcSchoolCollectionStudentRepository sdcSchoolCollectionStudentRepository;
     private JasperReport refugeeHeadcountReport;
+    private List<RefugeeHeadcountResult> refugeeHeadcounts = new ArrayList<>();
+    private final ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
 
-    protected RefugeeHeadcountPerSchoolReportService(RestUtils restUtils) {
+    protected RefugeeHeadcountPerSchoolReportService(SdcSchoolCollectionRepository sdcSchoolCollectionRepository, SdcDistrictCollectionRepository sdcDistrictCollectionRepository, SdcSchoolCollectionStudentRepository sdcSchoolCollectionStudentRepository, RestUtils restUtils) {
         super(restUtils);
+        this.sdcSchoolCollectionRepository = sdcSchoolCollectionRepository;
+        this.sdcDistrictCollectionRepository = sdcDistrictCollectionRepository;
+        this.sdcSchoolCollectionStudentRepository = sdcSchoolCollectionStudentRepository;
     }
 
     @PostConstruct
@@ -39,6 +59,34 @@ public class RefugeeHeadcountPerSchoolReportService extends BaseReportGeneration
         } catch (JRException e) {
             throw new StudentDataCollectionAPIRuntimeException("Compiling Jasper reports has failed :: " + e.getMessage());
         }
+    }
+
+    public DownloadableReportResponse generateRefugeePerSchoolReport(UUID collectionID){
+        try {
+            Optional<SdcDistrictCollectionEntity> sdcDistrictCollectionEntityOptional =  sdcDistrictCollectionRepository.findById(collectionID);
+            SdcDistrictCollectionEntity sdcDistrictCollectionEntity = sdcDistrictCollectionEntityOptional.orElseThrow(() ->
+                    new EntityNotFoundException(SdcDistrictCollectionEntity.class, "CollectionId", collectionID.toString()));
+
+            refugeeHeadcounts = sdcSchoolCollectionStudentRepository.getRefugeeHeadcountsBySdcDistrictCollectionIdGroupBySchoolId(sdcDistrictCollectionEntity.getSdcDistrictCollectionID());
+            return generateJasperReport(convertToRefugeeReportJSONStringDistrict(refugeeHeadcounts, sdcDistrictCollectionEntity), refugeeHeadcountReport, ReportTypeCode.DIS_REFUGEE_HEADCOUNT_PER_SCHOOL);
+        } catch (JsonProcessingException e) {
+            log.error("Exception occurred while writing PDF report for band of residence report :: " + e.getMessage());
+            throw new StudentDataCollectionAPIRuntimeException("Exception occurred while writing PDF report for band of residence report :: " + e.getMessage());
+        }
+    }
+
+    private String convertToRefugeeReportJSONStringDistrict(List<RefugeeHeadcountResult> mappedResults, SdcDistrictCollectionEntity sdcDistrictCollection) throws JsonProcessingException {
+        HeadcountNode mainNode = new HeadcountNode();
+        HeadcountReportNode reportNode = new HeadcountReportNode();
+        setReportTombstoneValuesDis(sdcDistrictCollection, reportNode);
+
+        var nodeMap = generateNodeMap(false);
+
+        mappedResults.forEach(result -> setValueForGrade(nodeMap, result));
+
+        reportNode.setPrograms(nodeMap.values().stream().sorted(Comparator.comparing(o -> Integer.parseInt(o.getSequence()))).toList());
+        mainNode.setReport(reportNode);
+        return objectWriter.writeValueAsString(mainNode);
     }
 
     protected HashMap<String, HeadcountChildNode> generateNodeMap(boolean includeKH) {
