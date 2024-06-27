1e840779f595ef99bbcaeb85028ab0351633e01e
Eckerman <erineckerman@gmail.com>
2024-06-26 17:16:13 -0700

    fix flyway script

--- api/src/main/resources/db/migration/V1.0.68__STUDENT_DATA_COLLECTION.API.sql
+++ api/src/main/resources/db/migration/V1.0.68__STUDENT_DATA_COLLECTION.API.sql
@@ -1,2 +1,5 @@
 ALTER TABLE sdc_duplicate_student ALTER COLUMN sdc_district_collection_id DROP NOT NULL;
-ALTER TABLE sdc_duplicate add collection_id UUID NOT NULL CONSTRAINT FK_COLLECTION_ID foreign key (COLLECTION_ID) references COLLECTION (COLLECTION_ID);
+
+ALTER TABLE sdc_duplicate
+    ADD COLUMN collection_id UUID NOT NULL,
+ADD CONSTRAINT FK_COLLECTION_ID FOREIGN KEY (collection_id) REFERENCES COLLECTION (COLLECTION_ID);
