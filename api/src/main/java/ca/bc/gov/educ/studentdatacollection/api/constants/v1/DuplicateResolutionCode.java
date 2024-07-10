4d026f60f783dcc157896f5c15db676363c63096
Marco Villeneuve <arcshiftsolutions@gmail.com>
2024-07-09 17:02:41 -0700

    Fix for duplicate resolution

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/constants/v1/DuplicateResolutionCode.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/constants/v1/DuplicateResolutionCode.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 public enum DuplicateResolutionCode {
 
-  RELEASED("RELEASED", "Student released from school."),
+  RELEASED("RELEASED", "Student removed from school."),
   GRADE_CHNG("GRADE_CHNG", "Student grade was changed."),
   RESOLVED("RESOLVED", "Student removed from conflicting program.");
 
