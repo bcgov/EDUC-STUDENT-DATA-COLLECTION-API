7c3769f9e9ae852e2394bfc9eb1a88f828544836
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2024-07-25 09:26:46 -0700

    Create UpdateStudentSagaData.java

---
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/struct/UpdateStudentSagaData.java
@@ -0,0 +1,28 @@
+package ca.bc.gov.educ.studentdatacollection.api.struct;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UpdateStudentSagaData {
+    String sdcSchoolCollectionStudentID;
+    String assignedPEN;
+    String dob;
+    String sexCode;
+    String genderCode;
+    String usualFirstName;
+    String usualMiddleNames;
+    String usualLastName;
+    String postalCode;
+    String mincode;
+    String localID;
+    String gradeCode;
+
+}
