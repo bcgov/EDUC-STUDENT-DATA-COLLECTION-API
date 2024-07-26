101222a0bdffcb2431ec3cca0bc4c06e19240567
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2024-07-26 09:37:18 -0700

    remove todo

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/schedulers/EventTaskScheduler.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/schedulers/EventTaskScheduler.java
@@ -64,7 +64,6 @@ public class EventTaskScheduler {
     this.getTaskSchedulerAsyncService().findAllUnsubmittedIndependentSchoolsInCurrentCollection();
   }
 
-  // todo only run scheduler if before the posting of prov dupes
   @Scheduled(cron = "${scheduled.jobs.process.new.schools.collection.cron}")
   @SchedulerLock(name = "ADD_NEW_SCHOOLS_TO_COLLECTION", lockAtLeastFor = "${scheduled.jobs.process.new.schools.collection.cron.lockAtLeastFor}", lockAtMostFor = "${scheduled.jobs.process.new.schools.collection.cron.lockAtMostFor}")
   public void findNewSchoolsAndAddSdcSchoolCollection() {
