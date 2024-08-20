4ef739168d73e66e1a029ce54ddd080338e74532
keeganbcgov <Keegan.Laur@gov.bc.ca>
2024-08-20 16:20:37 -0700

    EDX-2861: ensuring v76 doesn't execute in July collections.

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/rules/validationrules/impl/OutOfProvinceSpecialEducRule.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/rules/validationrules/impl/OutOfProvinceSpecialEducRule.java
@@ -4,7 +4,7 @@ import ca.bc.gov.educ.studentdatacollection.api.calculator.FteCalculatorUtils;
 import ca.bc.gov.educ.studentdatacollection.api.constants.StudentValidationFieldCode;
 import ca.bc.gov.educ.studentdatacollection.api.constants.StudentValidationIssueSeverityCode;
 import ca.bc.gov.educ.studentdatacollection.api.constants.StudentValidationIssueTypeCode;
-import ca.bc.gov.educ.studentdatacollection.api.constants.v1.FacilityTypeCodes;
+import ca.bc.gov.educ.studentdatacollection.api.constants.v1.CollectionTypeCodes;
 import ca.bc.gov.educ.studentdatacollection.api.constants.v1.SchoolCategoryCodes;
 import ca.bc.gov.educ.studentdatacollection.api.constants.v1.SchoolFundingCodes;
 import ca.bc.gov.educ.studentdatacollection.api.rules.ValidationBaseRule;
@@ -38,7 +38,7 @@ public class OutOfProvinceSpecialEducRule implements ValidationBaseRule {
         var shouldExecute = isValidationDependencyResolved("V76", validationErrorsMap) &&
                 SchoolFundingCodes.OUT_OF_PROVINCE.getCode().equals(studentRuleData.getSdcSchoolCollectionStudentEntity().getSchoolFundingCode()) &&
                 studentRuleData.getSchool().getSchoolCategoryCode().equalsIgnoreCase(SchoolCategoryCodes.PUBLIC.getCode()) &&
-                !studentRuleData.getSchool().getFacilityTypeCode().equalsIgnoreCase(FacilityTypeCodes.SUMMER.getCode());
+                !FteCalculatorUtils.getCollectionTypeCode(studentRuleData).equalsIgnoreCase(CollectionTypeCodes.JULY.getTypeCode());
 
         log.debug("In shouldExecute of OutOfProvinceSpecialEducRule-V76: Condition returned  - {} for sdcSchoolCollectionStudentID :: {}" ,
                 shouldExecute,
