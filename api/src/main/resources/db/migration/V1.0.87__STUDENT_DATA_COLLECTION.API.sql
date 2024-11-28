fd97d6caf29d3149aa9f3b396e37b9ad6433855c
Marco Villeneuve <arcshiftsolutions@gmail.com>
2024-11-28 13:30:42 -0800

    Fix for performance on migration step

--- api/src/main/resources/db/migration/V1.0.87__STUDENT_DATA_COLLECTION.API.sql
+++ api/src/main/resources/db/migration/V1.0.87__STUDENT_DATA_COLLECTION.API.sql
@@ -1,2 +1,2 @@
-DROP CONSTRAINT sdc_saga_event_states_saga_id_fk;
+alter table SDC_SAGA_EVENT_STATES DROP CONSTRAINT sdc_saga_event_states_saga_id_fk;
 ALTER TABLE SDC_SAGA_EVENT_STATES ADD CONSTRAINT sdc_saga_event_states_saga_id_fk FOREIGN KEY (saga_id) REFERENCES sdc_saga(saga_id) on delete cascade;
