91cc861c0de168f017a711aa5c434679ce47faf5
QSL\SumathiT <sumathi.thirumani@quartech.com>
2024-12-24 10:27:31 -0800

    Remove unused import.

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/repository/v1/SagaRepository.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/repository/v1/SagaRepository.java
@@ -2,7 +2,6 @@ package ca.bc.gov.educ.studentdatacollection.api.repository.v1;
 
 
 import ca.bc.gov.educ.studentdatacollection.api.model.v1.SdcSagaEntity;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
