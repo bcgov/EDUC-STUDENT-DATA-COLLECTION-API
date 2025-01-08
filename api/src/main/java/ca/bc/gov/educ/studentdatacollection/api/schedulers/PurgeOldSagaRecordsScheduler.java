2654be5d40808267cd479a3547c22e0b4247089d
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2025-01-08 11:54:45 -0800

    Update PurgeOldSagaRecordsScheduler.java

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/schedulers/PurgeOldSagaRecordsScheduler.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/schedulers/PurgeOldSagaRecordsScheduler.java
@@ -4,16 +4,12 @@ import ca.bc.gov.educ.studentdatacollection.api.repository.v1.SagaEventRepositor
 import ca.bc.gov.educ.studentdatacollection.api.repository.v1.SagaRepository;
 import jakarta.transaction.Transactional;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.core.LockAssert;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
-import org.apache.commons.collections4.CollectionUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
