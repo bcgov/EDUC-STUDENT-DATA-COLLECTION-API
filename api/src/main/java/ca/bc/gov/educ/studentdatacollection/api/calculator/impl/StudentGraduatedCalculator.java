90f9be028ac76db10af5bf1f66e6479985640305
Marco Villeneuve <arcshiftsolutions@gmail.com>
2025-01-28 15:24:39 -0800

    Updates to support block rules

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/calculator/impl/StudentGraduatedCalculator.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/calculator/impl/StudentGraduatedCalculator.java
@@ -1,6 +1,7 @@
 package ca.bc.gov.educ.studentdatacollection.api.calculator.impl;
 
 import ca.bc.gov.educ.studentdatacollection.api.calculator.FteCalculator;
+import ca.bc.gov.educ.studentdatacollection.api.constants.v1.FacilityTypeCodes;
 import ca.bc.gov.educ.studentdatacollection.api.constants.v1.SchoolCategoryCodes;
 import ca.bc.gov.educ.studentdatacollection.api.struct.StudentRuleData;
 import ca.bc.gov.educ.studentdatacollection.api.struct.v1.FteCalculationResult;
@@ -34,14 +35,23 @@ public class StudentGraduatedCalculator implements FteCalculator {
         if (Boolean.TRUE.equals(studentData.getSdcSchoolCollectionStudentEntity().getIsGraduated())) {
             log.debug("StudentGraduatedCalculator: calculating for a graduated student :: " + studentData.getSdcSchoolCollectionStudentEntity().getSdcSchoolCollectionStudentID());
             fteCalculationResult.setFte(numCourses.multiply(fteMultiplier).setScale(4, RoundingMode.HALF_UP).stripTrailingZeros());
-            if(SchoolCategoryCodes.INDEPENDENTS.contains(studentData.getSchool().getSchoolCategoryCode())) {
+            if(SchoolCategoryCodes.INDEPENDENTS.contains(studentData.getSchool().getSchoolCategoryCode()) && FacilityTypeCodes.STANDARD.getCode().equals(studentData.getSchool().getFacilityTypeCode())) {
                 fteCalculationResult.setFte(fteCalculationResult.getFte().compareTo(BigDecimal.ONE) > 0 ? BigDecimal.ONE : fteCalculationResult.getFte());
             }
         } else {
             log.debug("StudentGraduatedCalculator: calculating for a non-graduated student :: " + studentData.getSdcSchoolCollectionStudentEntity().getSdcSchoolCollectionStudentID());
-            BigDecimal numSupportBlocks = new BigDecimal(studentData.getSdcSchoolCollectionStudentEntity().getSupportBlocks());
-            BigDecimal fte = (numCourses.add(numSupportBlocks).multiply(fteMultiplier)).setScale(4, RoundingMode.HALF_UP).stripTrailingZeros();
-            fteCalculationResult.setFte(fte.compareTo(BigDecimal.ONE) > 0 ? BigDecimal.ONE : fte);
+            BigDecimal fte = (numCourses.multiply(fteMultiplier).setScale(4, RoundingMode.HALF_UP).stripTrailingZeros());
+            fteCalculationResult.setFte(fte);
+
+            if(fte.compareTo(BigDecimal.ONE) < 0){
+                BigDecimal numSupportBlocks = new BigDecimal(studentData.getSdcSchoolCollectionStudentEntity().getSupportBlocks());
+                fte = (numCourses.add(numSupportBlocks).multiply(fteMultiplier)).setScale(4, RoundingMode.HALF_UP).stripTrailingZeros();
+                fteCalculationResult.setFte(fte.compareTo(BigDecimal.ONE) > 0 ? BigDecimal.ONE : fte);
+            }
+
+            if(SchoolCategoryCodes.INDEPENDENTS.contains(studentData.getSchool().getSchoolCategoryCode()) && FacilityTypeCodes.STANDARD.getCode().equals(studentData.getSchool().getFacilityTypeCode())) {
+                fteCalculationResult.setFte(fte.compareTo(BigDecimal.ONE) > 0 ? BigDecimal.ONE : fte);
+            }
         }
         if (fteCalculationResult.getFte() != null && fteCalculationResult.getFte().compareTo(BigDecimal.ZERO) > 0) {
             fteCalculationResult.setFteZeroReason(null);
