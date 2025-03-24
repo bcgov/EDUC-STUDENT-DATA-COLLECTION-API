4b360ba45922f655e1417d58d0f83fa083d210c6
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2025-03-24 12:31:20 -0700

    Revert "pagination"
    This reverts commit fa9e22b4687501d127cde3874e948e91936c4c11.

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/repository/v1/SdcSchoolCollectionStudentFsaReportRepository.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/repository/v1/SdcSchoolCollectionStudentFsaReportRepository.java
@@ -1,8 +1,6 @@
 package ca.bc.gov.educ.studentdatacollection.api.repository.v1;
 
 import ca.bc.gov.educ.studentdatacollection.api.model.v1.SdcSchoolCollectionStudentFsaReportEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -13,5 +11,4 @@ import java.util.UUID;
 @Repository
 public interface SdcSchoolCollectionStudentFsaReportRepository extends JpaRepository<SdcSchoolCollectionStudentFsaReportEntity, UUID>, JpaSpecificationExecutor<SdcSchoolCollectionStudentFsaReportEntity> {
     List<SdcSchoolCollectionStudentFsaReportEntity> findAllBySdcSchoolCollection_CollectionIDAndEnrolledGradeCodeInAndSdcSchoolCollectionStudentStatusCodeIsNot(UUID collectionID, List<String> enrolledGradeCode, String sdcSchoolCollectionStudentStatusCode);
-    Page<SdcSchoolCollectionStudentFsaReportEntity> findAllBySdcSchoolCollection_CollectionIDAndEnrolledGradeCodeInAndSdcSchoolCollectionStudentStatusCodeIsNot(UUID collectionID, List<String> enrolledGradeCode, String sdcSchoolCollectionStudentStatusCode, Pageable pageable);
 }
