88a79046ada0d2af5c0535c91c63b81c5d40465b
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2025-04-01 10:44:16 -0700

    Report change: FSA Registration Report

--- api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/constants/v1/ministryreports/FsaFebRegistrationHeader.java
+++ api/src/main/java/ca/bc/gov/educ/studentdatacollection/api/constants/v1/ministryreports/FsaFebRegistrationHeader.java
@@ -7,7 +7,7 @@ public enum FsaFebRegistrationHeader {
 
     MINCODE("Mincode"),
     STUDENT_PEN("Student PEN"),
-    NEXT_YEAR_GRADE("Next Year Grade"),
+    NEXT_YEAR_GRADE("Grade"),
     LEGAL_FIRST_NAME("Legal First Name"),
     LEGAL_LAST_NAME("Legal Last Name");
 
